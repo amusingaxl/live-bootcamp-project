c57bdc2fc226a0661e6ca05135ae41d0a797bab9
amusingaxl <112016538+amusingaxl@users.noreply.github.com>
2026-01-24 16:13:09 -0300

    [Sprint 1][Task 1] refactor: update docker hub user; update /hello response payload

--- auth-service/src/main.rs
+++ auth-service/src/main.rs
@@ -14,10 +14,10 @@ async fn main() {
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     println!("listening on {}", listener.local_addr().unwrap());
 
-    axum::serve(listener, app).await.unwrap();
+    serve(listener, app).await.unwrap();
 }
 
 async fn hello_handler() -> Html<&'static str> {
     // TODO: Update this to a custom message!
-    Html("<h1>Hello, World!</h1>")
+    Html("<h1>Hello, Milky Way!</h1>")
 }
